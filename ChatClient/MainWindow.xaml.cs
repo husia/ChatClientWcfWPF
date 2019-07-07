6b55e75e19d63056e061c35707c84a068305da45
husia <husia.ru@mail.ru>
2019-07-08 01:42:18 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ChatClient.ServiceChat.IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChat.ServiceChatClient client;
+        int Id;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChat.ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+
+                Id =  client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bConnDisconn.Content = "Disconnect";
+                isConnected = true;
+            }
+        }
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(Id);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bConnDisconn.Content = "Connect";
+                isConnected = false;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void TbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.Key == Key.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMsg(": "+tbMessage.Text, Id);
+                    tbMessage.Text = " ";
+                }
+                }
+        }
+    }
+}
---
+++ WCF_Chat/IServiceChat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServiceChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+
+    public interface IServiceChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+         void MsgCallback(string msg);
+    }
+}
